4754f5212f187a6a2f20741524d08a4beee7c9e9
Leonard Mendonça <81590435+lqdm12@users.noreply.github.com>
2026-07-13 01:04:30 -0300

    Remove script tags from script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-<script>
 const menuBtn = document.getElementById('menuBtn');
   const mobileMenu = document.getElementById('mobileMenu');
   menuBtn?.addEventListener('click', () => {
@@ -6,6 +5,5 @@ const menuBtn = document.getElementById('menuBtn');
   });
   // close on link click
   mobileMenu?.querySelectorAll('a').forEach(a => a.addEventListener('click', () => mobileMenu.classList.add('hidden')));
-</script>
 
 (function(){document.addEventListener("click",function(e){var a=e.target.closest("[data-product-id]");if(!a)return;e.preventDefault();var pid=a.getAttribute("data-product-id");if(pid)parent.postMessage({type:"ecto-artifact-link-click",productId:pid},"*")})})();
